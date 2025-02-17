f86b6ef32ed702b99d996001fdc7971795d1e3ee
cchoijjinyoung <cgy7127@gmail.com>
2025-02-18 01:44:50 +0900

    refactor#9: CustomException 수정
    - 예외 케이스 테스트 코드 작성 중 body의 message에 null 값이 들어가서 수정했습니다.

--- src/main/java/com/fourseason/delivery/global/exception/CustomException.java
+++ src/main/java/com/fourseason/delivery/global/exception/CustomException.java
@@ -1,11 +1,14 @@
 package com.fourseason.delivery.global.exception;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
 
-    private final ErrorCode errorCode;
+  private final ErrorCode errorCode;
+
+  public CustomException(ErrorCode errorCode) {
+    super(errorCode.message());
+    this.errorCode = errorCode;
+  }
 }
