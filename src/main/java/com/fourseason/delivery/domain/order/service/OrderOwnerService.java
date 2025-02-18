011358d713defce536d2c23de78c222e4a8c7a58
cchoijjinyoung <cgy7127@gmail.com>
2025-02-18 20:39:10 +0900

    feat#13: 가게 Owner 주문 수락 API 구현

---
+++ src/main/java/com/fourseason/delivery/domain/order/service/OrderOwnerService.java
@@ -0,0 +1,30 @@
+package com.fourseason.delivery.domain.order.service;
+
+import static com.fourseason.delivery.domain.order.entity.OrderStatus.ACCEPTED;
+import static com.fourseason.delivery.domain.order.exception.OrderErrorCode.ORDER_NOT_FOUND;
+
+import com.fourseason.delivery.domain.order.entity.Order;
+import com.fourseason.delivery.domain.order.repository.OrderRepository;
+import com.fourseason.delivery.global.exception.CustomException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OrderOwnerService {
+
+  private final OrderRepository orderRepository;
+
+  @Transactional
+  public void acceptOrder(Long ownerId, UUID orderId) {
+
+    Order order = orderRepository.findById(orderId).
+        orElseThrow(() -> new CustomException(ORDER_NOT_FOUND));
+
+    order.validateShopOwner(ownerId);
+
+    order.updateStatus(ACCEPTED);
+  }
+}
