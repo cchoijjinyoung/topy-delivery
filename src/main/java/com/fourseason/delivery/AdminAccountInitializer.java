f0ea491b5f29732c804b1bb27af242f5ec1dcad3
lime1st <lime1st@gmail.com>
2025-02-19 10:59:28 +0900

    feat: MASTER 계정 추가

---
+++ src/main/java/com/fourseason/delivery/AdminAccountInitializer.java
@@ -0,0 +1,32 @@
+package com.fourseason.delivery;
+
+import com.fourseason.delivery.domain.member.entity.Member;
+import com.fourseason.delivery.domain.member.entity.Role;
+import com.fourseason.delivery.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminAccountInitializer implements CommandLineRunner {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (memberRepository.findByUsername("admin").isEmpty()) {
+            Member admin = new Member(
+                    "admin",
+                    "admin@delivery.com",
+                    passwordEncoder.encode("1234"),
+                    "010-1234-5678",
+                    Role.MASTER
+            );
+            memberRepository.save(admin);
+            System.out.println("Admin account created.");
+        }
+    }
+}
