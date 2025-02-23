7708a094452ba625fa3c295652d3516226cd2836
lime1st <lime1st@gmail.com>
2025-02-23 17:04:22 +0900

    comment: API 별 주석 수정 테스트용 api 삭제

--- src/main/java/com/fourseason/delivery/global/auth/controller/AuthController.java
+++ src/main/java/com/fourseason/delivery/global/auth/controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.fourseason.delivery.global.auth.controller;
 
 import com.fourseason.delivery.domain.member.entity.Role;
-import com.fourseason.delivery.global.auth.CustomPrincipal;
 import com.fourseason.delivery.global.auth.dto.TokenDto;
 import com.fourseason.delivery.global.auth.dto.request.SignInRequestDto;
 import com.fourseason.delivery.global.auth.dto.request.SignUpRequestDto;
@@ -12,8 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
