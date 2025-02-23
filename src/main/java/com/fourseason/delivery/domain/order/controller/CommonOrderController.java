874fa6b11ec378302552140c46768f589295ff92
cchoijjinyoung <cgy7127@gmail.com>
2025-02-23 19:01:01 +0900

    #52 feat: CommonOrderController, OrderRoleService, OrderRoleServiceFinder를 추가하여 권한 별 API 동작 분기처리
    - 권한 별로 '주문 상세조회', '주문 목록 조회', '주문 취소'는 각각 다르게 동작되어야한다고 판단했습니다.
    - OrderRoleService 인터페이스를 생성하고, 해당 인터페이스를 구현하는 구현체를 권한별로 생성하였습니다.
    - OrderRoleServiceFinder 클래스를 생성하여 빈 리스트 주입을 통해 권한별로 OrderRoleService를 찾아올 수 있도록 설계하였습니다.

---
+++ src/main/java/com/fourseason/delivery/domain/order/controller/CommonOrderController.java
@@ -0,0 +1,68 @@
+package com.fourseason.delivery.domain.order.controller;
+
+import com.fourseason.delivery.domain.order.dto.response.OrderDetailsResponseDto;
+import com.fourseason.delivery.domain.order.dto.response.OrderSummaryResponseDto;
+import com.fourseason.delivery.domain.order.service.OrderRoleServiceFinder;
+import com.fourseason.delivery.global.auth.CustomPrincipal;
+import com.fourseason.delivery.global.dto.PageRequestDto;
+import com.fourseason.delivery.global.dto.PageResponseDto;
+import jakarta.validation.constraints.NotBlank;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class CommonOrderController {
+
+  private final OrderRoleServiceFinder orderRoleServiceFinder;
+
+  /**
+   * 주문 상세 조회 API
+   */
+  @GetMapping("/{orderId}")
+  public ResponseEntity<OrderDetailsResponseDto> getOrder(
+      @PathVariable UUID orderId,
+      @AuthenticationPrincipal CustomPrincipal principal
+  ) {
+    return ResponseEntity.ok(
+        orderRoleServiceFinder.find(principal.getRole()).getOrder(orderId, principal.getId()));
+  }
+
+  /**
+   * 주문 목록 조회 API
+   */
+  @GetMapping
+  public ResponseEntity<PageResponseDto<? extends OrderSummaryResponseDto>> getOrderList(
+      @RequestParam @NotBlank(message = "유저네임을 입력해주세요.") String username,
+      @AuthenticationPrincipal CustomPrincipal principal,
+      @RequestParam(defaultValue = "1") int page,
+      @RequestParam(defaultValue = "10") int size,
+      @RequestParam(defaultValue = "latest") String orderBy,
+      @RequestParam(required = false) String keyword
+  ) {
+    PageRequestDto pageRequestDto = PageRequestDto.of(page - 1, size, orderBy);
+    return ResponseEntity.ok(orderRoleServiceFinder.find(principal.getRole())
+        .getOrderList(username, principal.getName(), pageRequestDto, keyword));
+  }
+
+  /**
+   * 주문 취소 API
+   */
+  @PostMapping("/{orderId}/cancel")
+  public ResponseEntity<Void> cancelOrder(
+      @PathVariable UUID orderId,
+      @AuthenticationPrincipal CustomPrincipal principal
+  ) {
+    orderRoleServiceFinder.find(principal.getRole()).cancelOrder(orderId, principal.getId());
+    return ResponseEntity.ok().build();
+  }
+}
