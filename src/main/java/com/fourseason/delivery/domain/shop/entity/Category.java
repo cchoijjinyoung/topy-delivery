e0311ff4a53e9d716d3fc564e39fbe973fb391a2
ss0ming <hee9920@naver.com>
2025-02-24 12:29:56 +0900

    refactor: category 기본 생성자 접근 제어를 PROTECTED로 변경

--- src/main/java/com/fourseason/delivery/domain/shop/entity/Category.java
+++ src/main/java/com/fourseason/delivery/domain/shop/entity/Category.java
@@ -2,8 +2,10 @@ package com.fourseason.delivery.domain.shop.entity;
 
 import com.fourseason.delivery.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
@@ -11,6 +13,7 @@ import java.util.UUID;
 @Entity
 @Getter
 @Table(name = "p_category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category extends BaseTimeEntity {
 
     @Id
