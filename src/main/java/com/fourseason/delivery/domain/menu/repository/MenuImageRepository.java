8f8edcc3cf3a4d317c939a345c863bb460b98a1e
ss0ming <hee9920@naver.com>
2025-02-24 14:15:57 +0900

    feat: menu 업데이트 시 image 기능 추가
    - 기존에 존재하는 이미지들은 List<UUID>로 요청 받음
    - 새로 업로드할 이미지들은 List<MultipartFile>로 요청 받음
    - 기존에 존재하는 이미지들과 데이터베이스에 저장된 이미지를 비교하여 없으면 삭제
    - 새로 올라온 이미지는 s3에 업로드

--- src/main/java/com/fourseason/delivery/domain/menu/repository/MenuImageRepository.java
+++ src/main/java/com/fourseason/delivery/domain/menu/repository/MenuImageRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.UUID;
 
 public interface MenuImageRepository extends JpaRepository<MenuImage, UUID> {
-    List<MenuImage> findByMenuId(UUID id);
+    List<MenuImage> findByMenuIdAndDeletedByIsNull(UUID id);
 }
